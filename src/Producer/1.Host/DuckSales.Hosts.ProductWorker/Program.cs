e8be31749820c4b5f02f1e33c6f7d1b8f87799d4
André Barsotti <andresalvadeo@hotmail.com>
2022-07-23 18:10:17 -0300

    Configuração básica do Serilog

--- src/Producer/1.Host/DuckSales.Hosts.ProductWorker/Program.cs
+++ src/Producer/1.Host/DuckSales.Hosts.ProductWorker/Program.cs
@@ -1,10 +1,47 @@
 using DuckSales.Hosts.ProductWorker;
+using Serilog;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom
+    .Configuration(GetConfiguration(args))
+    .CreateLogger();
 
-await host.RunAsync();
+try
+{
+    Log.Information("Starting host...");
+    IHost host = Host.CreateDefaultBuilder(args)
+        .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+        .UseSerilog((context, services, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration))
+        .Build();
+
+    Log.Information("Host created, running app...");
+    await host.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+static IConfiguration GetConfiguration(string[] args)
+{
+    string enviroment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
+
+    IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+    configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .AddJsonFile($"appsettings.{enviroment}.json", true);
+
+    if (enviroment == "Development")
+        configurationBuilder.AddUserSecrets<Program>(optional: true);
+
+    configurationBuilder.AddEnvironmentVariables()
+        .AddCommandLine(args);
+
+    return configurationBuilder.Build();
+}
+
