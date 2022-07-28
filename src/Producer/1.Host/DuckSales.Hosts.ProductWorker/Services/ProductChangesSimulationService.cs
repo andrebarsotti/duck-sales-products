d84e9b865a3493d7c4c662bc8069e01ea4dec463
Andre Salvadeo <andresalvadeo@hotmail.com>
2022-07-28 18:40:09 -0300

    Refatoração do Serviço ProductChangesSimulationService

--- src/Producer/1.Host/DuckSales.Hosts.ProductWorker/Services/ProductChangesSimulationService.cs
+++ src/Producer/1.Host/DuckSales.Hosts.ProductWorker/Services/ProductChangesSimulationService.cs
@@ -23,26 +23,44 @@ public sealed class ProductChangesSimulationService : IProductChangesSimulationS
 
     public async Task Execute()
     {
-        int amountOfProducts = await _mediator.Send(new GetTotalAmountOfProdcuntFromCatalogQuery());
-
-        if (_faker.Random.Double(min: 0D, max: 1D) <= GetChanceOfUpdate(amountOfProducts))
+        if (ShouldUpdateAProduct(await GetTotalAmountOfProdcuntFromCatalog()))
         {
-            IEnumerable<ProductOfCatalogReadModel> productList =
-                await _mediator.Send(new GetAllProductsFromCatalogQuery());
-
-            ProductOfCatalogReadModel product = _faker.PickRandom(productList);
-
-            await _mediator.Send(new UpdateProductCommand(product.ProductId,
-                _faker.Random.Int(0),
-                _faker.Finance.Amount()));
+            await UpdateAProduct();
         }
         else
         {
-            await _mediator.Send(new CreateProductCommand(_faker.Commerce.ProductName(),
-                _faker.Commerce.Department(),
-                _faker.Random.Int(0),
-                _faker.Finance.Amount()));
+            await CreateNewProduct();
         }
+    }
+
+    private async Task CreateNewProduct() =>
+        await _mediator.Send(new CreateProductCommand(_faker.Commerce.ProductName(),
+            _faker.Commerce.Department(),
+            _faker.Random.Int(0),
+            _faker.Finance.Amount()));
+
+    private async Task UpdateAProduct()
+    {
+        ProductOfCatalogReadModel product = await GetProductFromDataBase();
+
+        await _mediator.Send(new UpdateProductCommand(product.ProductId,
+            _faker.Random.Int(0),
+            _faker.Finance.Amount()));
+    }
+
+    private async Task<int> GetTotalAmountOfProdcuntFromCatalog() =>
+        await _mediator.Send(new GetTotalAmountOfProdcuntFromCatalogQuery());
+
+    private bool ShouldUpdateAProduct(int amountOfProducts) =>
+        _faker.Random.Double(0D, 1D) <= GetChanceOfUpdate(amountOfProducts);
+
+    private async Task<ProductOfCatalogReadModel> GetProductFromDataBase()
+    {
+        IEnumerable<ProductOfCatalogReadModel> productList =
+            await _mediator.Send(new GetAllProductsFromCatalogQuery());
+
+        ProductOfCatalogReadModel product = _faker.PickRandom(productList);
+        return product;
     }
 
     private double GetChanceOfUpdate(int amountOfProducts)
